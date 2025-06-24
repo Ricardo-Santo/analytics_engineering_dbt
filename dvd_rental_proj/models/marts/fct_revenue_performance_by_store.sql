aa2343d6a54764dd9034c82af85723daed266dd6
rodfernds <rodasahde@gmail.com>
2025-06-24 15:28:00 +0100

    Removed unnecessary comma before from clause, and ";" at the end of the query. Added information to the marts schema.yml.

--- dvd_rental_proj/models/marts/fct_revenue_performance_by_store.sql
+++ dvd_rental_proj/models/marts/fct_revenue_performance_by_store.sql
@@ -3,8 +3,8 @@
 SELECT
   store_id,
   sum(total_revenue) as total_revenue,
-  to_char(month, 'YYYY-MM') as month,
+  to_char(month, 'YYYY-MM') as month
 FROM {{ ref('int__dvd_rental_store_performance') }}
 WHERE month >= date '2007-01-01'
 GROUP BY store_id, month
-ORDER BY store_id, month;
+ORDER BY store_id, month
